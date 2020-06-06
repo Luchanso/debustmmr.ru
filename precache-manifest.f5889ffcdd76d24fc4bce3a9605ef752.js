9eb78f3c06acabd093ef3a381aae794576109f7b
Oleg Loutchansky <luchansodev@gmail.com>
2020-06-07 01:13:30 +0300

    Updates

---
+++ precache-manifest.f5889ffcdd76d24fc4bce3a9605ef752.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14440ed3dc4988ad8e293fa2ada0d9cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37f1ba119a7378371cbc",
+    "url": "/static/css/main.de071c1f.chunk.css"
+  },
+  {
+    "revision": "5a3b902e2964d24542ee",
+    "url": "/static/js/2.6e131a43.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6e131a43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37f1ba119a7378371cbc",
+    "url": "/static/js/main.b2350584.chunk.js"
+  },
+  {
+    "revision": "75e69cad7afab06386f7",
+    "url": "/static/js/runtime-main.59945b82.js"
+  },
+  {
+    "revision": "effe666b3aca0e8dbf3a4c134a883c59",
+    "url": "/static/media/main.effe666b.jpg"
+  }
+]);
